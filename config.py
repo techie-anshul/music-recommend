bb323586996d875a73a29a8da8a852eac49ad4e2
techie-anshul <anshulshriivastava@gmail.com>
2025-04-29 00:15:38 +0530

    Add Config and update data_loader file

---
+++ config.py
@@ -0,0 +1,14 @@
+# config.py
+
+DATA_FOLDER = "data"
+
+USERS_CSV = f"{DATA_FOLDER}/users.csv"
+EVENTS_CSV = f"{DATA_FOLDER}/events.csv"
+LOCATIONS_CSV = f"{DATA_FOLDER}/locations.csv"
+COUNTRIES_CSV = f"{DATA_FOLDER}/countries.csv"
+GENRES_CSV = f"{DATA_FOLDER}/genres.csv"
+EVENT_HISTORY_CSV = f"{DATA_FOLDER}/eventhistory.csv"
+FAVORITE_ARTISTS_CSV = f"{DATA_FOLDER}/favoriteartists.csv"
+FAVORITE_GENRES_CSV = f"{DATA_FOLDER}/favoritegenres.csv"
+EVENT_ARTISTS_CSV = f"{DATA_FOLDER}/eventartists.csv"
+ARTISTS_CSV = f"{DATA_FOLDER}/artists.csv"
--- data_loader.py
+++ data_loader.py
@@ -4,20 +4,19 @@ import pandas as pd
 from config import *
 
 def load_all_data():
-    # Re-load all required CSVs including the newly uploaded ones
-    users_df = pd.read_csv("C:/Users/arnav/Downloads/Compressed/archive_2/users.csv")
-    events_df = pd.read_csv("C:/Users/arnav/Downloads/Compressed/archive_2/events.csv")
-    locations_df = pd.read_csv("C:/Users/arnav/Downloads/Compressed/archive_2/locations.csv")
-    countries_df = pd.read_csv("C:/Users/arnav/Downloads/Compressed/archive_2/countries.csv")
-    genres_df = pd.read_csv("C:/Users/arnav/Downloads/Compressed/archive_2/genres.csv")
-    event_history_df = pd.read_csv("C:/Users/arnav/Downloads/Compressed/archive_2/eventhistory.csv")
-    favorite_artists_df = pd.read_csv("C:/Users/arnav/Downloads/Compressed/archive_2/favoriteartists.csv")
-    favorite_genres_df = pd.read_csv("C:/Users/arnav/Downloads/Compressed/archive_2/favoritegenres.csv")
-    event_artists_df = pd.read_csv("C:/Users/arnav/Downloads/Compressed/archive_2/eventartists.csv")
+    users_df = pd.read_csv(USERS_CSV)
+    events_df = pd.read_csv(EVENTS_CSV)
+    locations_df = pd.read_csv(LOCATIONS_CSV)
+    countries_df = pd.read_csv(COUNTRIES_CSV)
+    genres_df = pd.read_csv(GENRES_CSV)
+    event_history_df = pd.read_csv(EVENT_HISTORY_CSV)
+    favorite_artists_df = pd.read_csv(FAVORITE_ARTISTS_CSV)
+    favorite_genres_df = pd.read_csv(FAVORITE_GENRES_CSV)
+    event_artists_df = pd.read_csv(EVENT_ARTISTS_CSV)
 
     # Handle artists.csv manually
     parsed_rows = []
-    with open("C:/Users/arnav/Downloads/Compressed/archive_2/artists.csv", "r", encoding="utf-8") as f:
+    with open(ARTISTS_CSV, "r", encoding="utf-8") as f:
         for line in f:
             parts = line.strip().split(",")
             if len(parts) >= 3:
